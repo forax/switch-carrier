a86acdac8b6f086274e5a1d58b25ec0dcc6eb7e8
forax <forax@univ-mlv.fr>
2022-03-05 22:36:32 +0100

    add an example of pattern matching

---
+++ src/main/java/com/github/forax/carrier/java/lang/runtime/Patterns.java
@@ -0,0 +1,306 @@
+package com.github.forax.carrier.java.lang.runtime;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.RecordComponent;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+import static java.lang.invoke.MethodHandles.constant;
+import static java.lang.invoke.MethodHandles.dropArguments;
+import static java.lang.invoke.MethodHandles.filterArguments;
+import static java.lang.invoke.MethodHandles.filterReturnValue;
+import static java.lang.invoke.MethodHandles.foldArguments;
+import static java.lang.invoke.MethodHandles.guardWithTest;
+import static java.lang.invoke.MethodHandles.identity;
+import static java.lang.invoke.MethodHandles.insertArguments;
+import static java.lang.invoke.MethodHandles.lookup;
+import static java.lang.invoke.MethodHandles.permuteArguments;
+import static java.lang.invoke.MethodType.methodType;
+
+public class Patterns {
+  private static final MethodHandle THROW_NPE, INDEX_MINUS_ONE, IS_INSTANCE, IS_NULL;
+  static {
+    var lookup = lookup();
+    try {
+      THROW_NPE = lookup.findStatic(Patterns.class, "throwNPE", methodType(void.class, String.class));
+      INDEX_MINUS_ONE = lookup.findStatic(Patterns.class, "indexMinusOne", methodType(boolean.class, int.class));
+      IS_INSTANCE = lookup.findStatic(Patterns.class, "isInstance", methodType(boolean.class, Class.class, Object.class));
+      IS_NULL = lookup.findStatic(Patterns.class, "isNull", methodType(boolean.class, Object.class));
+    } catch (NoSuchMethodException | IllegalAccessException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  private static void throwNPE(String message) {
+    throw new NullPointerException(message);
+  }
+
+  private static boolean indexMinusOne(int index) {
+    return index == -1;
+  }
+
+  private static boolean isInstance(Class<?> type, Object o) {
+    return type.isInstance(o);
+  }
+
+  private static boolean isNull(Object o) {
+    return o == null;
+  }
+
+  // a pattern is typed
+  // (T;Ljava/lang/Object)Ljava/lang/Object;
+
+
+  private static void checkPattern(MethodHandle pattern) {
+    var type = pattern.type();
+    if (type.parameterCount() != 2 ||
+        type.returnType() != Object.class ||
+        type.parameterType(1) != Object.class) {
+      throw new IllegalArgumentException("invalid pattern " + pattern);
+    }
+  }
+
+  // return o -> pattern.apply(o, carrier);
+  public static MethodHandle of(Object emptyCarrier, MethodHandle pattern) {
+    Objects.requireNonNull(emptyCarrier, "emptyCarrier is null");
+    Objects.requireNonNull(pattern, "pattern is null");
+    checkPattern(pattern);
+    return insertArguments(pattern, 1, emptyCarrier);
+  }
+
+  // return (o, carrier) -> carrier;
+  public static MethodHandle match(Class<?> type, CarrierMetadata carrierMetadata, int index) {
+    Objects.requireNonNull(type, "type is null");
+    return dropArguments(insertArguments(carrierMetadata.with(0), 0, index), 0, type);
+
+  }
+
+  // return (o, carrier) -> null;
+  public static MethodHandle do_not_match(Class<?> type, CarrierMetadata carrierMetadata) {
+    return match(type, carrierMetadata, -1);
+  }
+
+  public static MethodHandle is_instance(Class<?> declaredType, Class<?> type) {
+    Objects.requireNonNull(declaredType, "declaredType is null");
+    Objects.requireNonNull(type, "type is null");
+    return dropArguments(IS_INSTANCE.bindTo(type).asType(methodType(boolean.class, declaredType)), 1, Object.class);
+  }
+
+  public static MethodHandle is_null(Class<?> type) {
+    Objects.requireNonNull(type, "type is null");
+    return dropArguments(IS_NULL.asType(methodType(boolean.class, type)), 1, Object.class);
+  }
+
+  public static MethodHandle index(Class<?> type, int index) {
+    Objects.requireNonNull(type, "type is null");
+    return dropArguments(constant(int.class, index), 0, type, Object.class);
+  }
+
+  // return (o, carrier) -> { throw new NullPointerException(); };
+  public static MethodHandle throw_NPE(Class<?> type, String message) {
+    Objects.requireNonNull(type, "type is null");
+    return dropArguments(THROW_NPE.bindTo(message).asType(methodType(Object.class)), 0, type, Object.class);
+  }
+
+  // return (o, carrier) -> pattern.apply(project.apply(o), carrier);
+  public static MethodHandle project(MethodHandle project, MethodHandle  pattern) {
+    Objects.requireNonNull(project, "project is null");
+    Objects.requireNonNull(pattern, "pattern is null");
+    var projectType = project.type();
+    if (projectType.parameterCount() != 1 ||
+        projectType.returnType() != pattern.type().parameterType(0)) {
+      throw new IllegalArgumentException("invalid project " + project + " for pattern " + pattern);
+    }
+    checkPattern(pattern);
+    return filterArguments(pattern, 0, project);
+  }
+
+  // return (o, carrier) -> pattern.apply(o, with(o, carrier, binding));
+  public static MethodHandle bind(int binding, CarrierMetadata carrierMetadata, MethodHandle pattern) {
+    Objects.requireNonNull(carrierMetadata, "carrierInfo is null");
+    Objects.requireNonNull(pattern, "pattern is null");
+    if (binding < 0) {
+      throw new IllegalArgumentException("binding negative " + binding);
+    }
+    checkPattern(pattern);
+    var mh = permuteArguments(pattern, methodType(Object.class, Object.class, pattern.type().parameterType(0), Object.class), 1, 0 );
+    return foldArguments(mh, carrierMetadata.with(binding));
+  }
+
+  // return (o, carrier) -> test.test(o, carrier)? target.apply(o, carrier): fallback.apply(o, carrier);
+  static MethodHandle test(MethodHandle test, MethodHandle target, MethodHandle fallback) {
+    Objects.requireNonNull(test, "test is null");
+    Objects.requireNonNull(target, "target is null");
+    Objects.requireNonNull(fallback, "fallback is null");
+    checkPattern(target);
+    checkPattern(fallback);
+    return guardWithTest(test, target, fallback);
+  }
+
+  // return (o, carrier) -> {
+  //      var carrier2 = pattern1.apply(o, carrier);
+  //      if (carrier2.accessor[Ø] == -1) {
+  //        return carrier2;
+  //      }
+  //      return pattern2.apply(o, carrier2);
+  //    };
+  static MethodHandle or(MethodHandle  pattern1, MethodHandle  pattern2, CarrierMetadata carrierMetadata) {
+    Objects.requireNonNull(pattern1, "pattern1 is null");
+    Objects.requireNonNull(pattern2, "pattern2 is null");
+    checkPattern(pattern1);
+    checkPattern(pattern2);
+
+    var type = pattern1.type().parameterType(0);
+    var test = filterReturnValue(dropArguments(carrierMetadata.accessor(0), 0, type), INDEX_MINUS_ONE);
+    var identity = dropArguments(identity(Object.class), 0, type);
+    var guard = guardWithTest(test, identity, pattern2);
+    var mh = permuteArguments(guard, methodType(Object.class, Object.class, type, Object.class), 1, 0 );
+    return foldArguments(mh, pattern1);
+  }
+
+  public record CarrierMetadata(Object empty, MethodHandle[] accessors, MethodHandle[] withers) {
+    public CarrierMetadata(MethodType carrierType) {
+      this(empty(carrierType), Carrier.components(carrierType), withers(carrierType));
+    }
+
+    public MethodHandle accessor(int i) {
+      return accessors[i];
+    }
+
+    // return a mh that creates a new carrier from a value and a previous value of a carrier
+    public MethodHandle with(int i) {
+      return withers[i];
+    }
+
+    private static Object empty(MethodType carrierType) {
+      try {
+        return MethodHandles.insertArguments(Carrier.constructor(carrierType), 0,
+                Arrays.stream(Carrier.components(carrierType))
+                    .map(c -> {
+                      var type = c.type().returnType();
+                      if (!type.isPrimitive()) {
+                        return null;
+                      }
+                      return switch (type.descriptorString()) {
+                        case "Z" -> false;
+                        case "B" -> (byte) 0;
+                        case "S" -> (short) 0;
+                        case "C" -> '\0';
+                        case "I" -> 0;
+                        case "J" -> 0L;
+                        case "F" -> 0f;
+                        case "D" -> 0.;
+                        default -> throw new AssertionError();
+                      };
+                    })
+                    .toArray(Object[]::new))
+            .invokeExact();
+      } catch (RuntimeException | Error e) {
+        throw e;
+      }catch (Throwable e) {
+        throw new AssertionError(e);
+      }
+    }
+
+    private static MethodHandle[] withers(MethodType carrierType) {
+      var constructor = Carrier.constructor(carrierType);
+      var accessors = Carrier.components(carrierType);
+      return IntStream.range(0, accessors.length)
+          .mapToObj(i -> with(constructor, accessors, i))
+          .toArray(MethodHandle[]::new);
+    }
+
+    private static MethodHandle with(MethodHandle constructor, MethodHandle[] accessors, int binding) {
+      var filters = new MethodHandle[accessors.length];
+      var reorder = new int[filters.length];
+      for(var i = 0; i < filters.length; i++) {
+        filters[i] = (i == binding)? null: accessors[i];
+        reorder[i] = (i == binding)? 0: 1;
+      }
+      var mh = filterArguments(constructor, 0, filters);
+      var bindingType = accessors[binding].type().returnType();
+      return MethodHandles.permuteArguments(mh, MethodType.methodType(Object.class, bindingType, Object.class), reorder);
+    }
+  }
+
+  public static MethodHandle cast(Class<?> type, MethodHandle pattern) {
+    Objects.requireNonNull(pattern);
+    checkPattern(pattern);
+    return pattern.asType(methodType(Object.class, type, Object.class));
+  }
+
+  private static final ClassValue<RecordComponent[]> RECORD_COMPONENTS = new ClassValue<>() {
+    @Override
+    protected RecordComponent[] computeValue(Class<?> type) {
+      if (!type.isRecord()) {
+        throw new IllegalArgumentException(type.getName() + " is not a record");
+      }
+      return type.getRecordComponents();
+    }
+  };
+
+  private static MethodHandle record_accessor(Lookup lookup, Class<?> recordClass, int position) {
+    var accessor = RECORD_COMPONENTS.get(recordClass)[position].getAccessor();
+    try {
+      return lookup.unreflect(accessor);
+    } catch (IllegalAccessException e) {
+      throw (LinkageError) new LinkageError().initCause(e);
+    }
+  }
+
+  public static void main(String[] args) throws Throwable {
+    record Point(int x, int y) {}
+    record Rectangle(Point p1, Point p2) {}
+
+    // Object o = ...
+    //switch(o) {
+    //  case Rectangle(Point p1, Point p2) -> ...
+    //}
+
+    var lookup = MethodHandles.lookup();
+
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class, Point.class, Point.class));
+
+    var empty = carrierMetadata.empty();
+
+    var op = of(empty,
+        test(is_instance(Object.class, Rectangle.class),
+            cast(Object.class,
+                or(
+                    project(record_accessor(lookup, Rectangle.class, 0),
+                        test(is_null(Point.class),
+                            do_not_match(Point.class, carrierMetadata),
+                            bind(1, carrierMetadata,
+                                match(Point.class, carrierMetadata, 0)))),
+                    project(record_accessor(lookup, Rectangle.class, 1),
+                        test(is_null(Point.class),
+                            do_not_match(Point.class, carrierMetadata),
+                            bind(2, carrierMetadata,
+                                match(Point.class, carrierMetadata, 0)))),
+                    carrierMetadata
+                )
+            ),
+            throw_NPE(Object.class, "o is null")
+        )
+    );
+
+    // match: new Rectangle(new Point(1, 2), new Point(3, 4))
+    var rectangle1 = (Object) new Rectangle(new Point(1, 2), new Point(3, 4));
+    var carrier1 = op.invokeExact(rectangle1);
+    System.out.println("result: " + (int) carrierMetadata.accessor(0).invokeExact(carrier1));
+    System.out.println("binding 1 " + (Point) carrierMetadata.accessor(1).invokeExact(carrier1));
+    System.out.println("binding 2 " + (Point) carrierMetadata.accessor(2).invokeExact(carrier1));
+
+    // match: new Rectangle(new Point(1, 2), new Point(3, 4))
+    var rectangle2 = (Object) new Rectangle(new Point(1, 2), null);
+    var carrier2 = op.invokeExact(rectangle2);
+    System.out.println("result: " + (int) carrierMetadata.accessor(0).invokeExact(carrier2));
+    System.out.println("binding 1 " + (Point) carrierMetadata.accessor(1).invokeExact(carrier2));
+    System.out.println("binding 2 " + (Point) carrierMetadata.accessor(2).invokeExact(carrier2));
+
+  }
+}
---
+++ src/test/java/com/github/forax/carrier/java/lang/runtime/PatternsTest.java
@@ -0,0 +1,189 @@
+package com.github.forax.carrier.java.lang.runtime;
+
+import com.github.forax.carrier.java.lang.runtime.Patterns.CarrierMetadata;
+import org.junit.jupiter.api.Test;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static java.lang.invoke.MethodType.methodType;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PatternsTest {
+
+  /*
+  private static MethodHandle asMH(BiFunction<?, ?, ?> op, Class<?> returnType, Class<?> parameterType1, Class<?> parameterType2) throws NoSuchMethodException, IllegalAccessException {
+    return MethodHandles.lookup()
+        .bind(op, "apply", methodType(Object.class, Object.class, Object.class))
+        .asType(methodType(returnType, parameterType1, parameterType2));
+  }*/
+
+  private static MethodHandle asMH(Function<?, ?> op, Class<?> returnType, Class<?> parameterType) throws NoSuchMethodException, IllegalAccessException {
+    return MethodHandles.lookup()
+        .bind(op, "apply", methodType(Object.class, Object.class))
+        .asType(methodType(returnType, parameterType));
+  }
+
+  private static MethodHandle asMH(Predicate<?> op, Class<?> parameterType) throws NoSuchMethodException, IllegalAccessException {
+    return MethodHandles.lookup()
+        .bind(op, "test", methodType(boolean.class, Object.class))
+        .asType(methodType(boolean.class, parameterType));
+  }
+
+  @Test
+  public void empty() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class, Object.class));
+    var carrier = carrierMetadata.empty();
+
+    assertEquals(0, (int) carrierMetadata.accessor(0).invokeExact(carrier));
+    assertNull(carrierMetadata.accessor(1).invokeExact(carrier));
+  }
+
+  @Test
+  public void match() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.match(int.class, carrierMetadata, 42);
+    var carrier2 = (Object) pattern.invokeExact(666, carrier);
+    var result = (int) carrierMetadata.accessor(0).invokeExact(carrier2);
+    assertEquals(42, result);
+  }
+
+  @Test
+  public void do_not_match() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.do_not_match(int.class, carrierMetadata);
+    var carrier2 = (Object) pattern.invokeExact(3, carrier);
+    var result = (int) carrierMetadata.accessor(0).invokeExact(carrier2);
+    assertEquals(-1, result);
+  }
+
+  @Test
+  public void is_instance() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.is_instance(Object.class, String.class);
+    var result = (boolean) pattern.invokeExact((Object) "data", carrier);
+    assertTrue(result);
+
+    var result2 = (boolean) pattern.invokeExact((Object) 777, carrier);
+    assertFalse(result2);
+  }
+
+  @Test
+  public void index() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.index(String.class, 3);
+    var result = (int) pattern.invokeExact("data", carrier);
+    assertEquals(3, result);
+  }
+
+  @Test
+  public void throw_NPE() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.throw_NPE(String.class, "achtung !");
+    assertThrows(NullPointerException.class, () -> {
+      var result = (Object) pattern.invokeExact("data", carrier);
+    });
+  }
+
+  @Test
+  public void project() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.project(asMH((String s) -> s.length(), int.class, String.class), Patterns.match(int.class, carrierMetadata, 42));
+    var carrier2 = pattern.invokeExact("hello", carrier);
+    var result = (int) carrierMetadata.accessor(0).invokeExact(carrier2);
+    assertEquals(42, result);
+  }
+
+  @Test
+  public void with() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var mh = carrierMetadata.with(0);
+    var carrier2 = mh.invokeExact(666, carrier);
+    var result = (int) carrierMetadata.accessor(0).invokeExact(carrier2);
+    assertEquals(666, result);
+  }
+
+  @Test
+  public void bind() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class, String.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.bind(1, carrierMetadata, Patterns.match(String.class, carrierMetadata, 42));
+    var carrier2 = pattern.invokeExact("hello", carrier);
+    var result = (int) carrierMetadata.accessor(0).invokeExact(carrier2);
+    var binding = (String) carrierMetadata.accessor(1).invokeExact(carrier2);
+    assertEquals(42, result);
+    assertEquals("hello", binding);
+  }
+
+  @Test
+  public void test() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.test(asMH((String s) -> s.startsWith("foo"), String.class),
+        Patterns.match(String.class, carrierMetadata, 0),
+        Patterns.do_not_match(String.class, carrierMetadata));
+    var carrier2 = pattern.invokeExact("foobar", carrier);
+    var index = (int) carrierMetadata.accessor(0).invokeExact(carrier2);
+    assertEquals(0, index);
+  }
+
+  @Test
+  public void or() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.or(
+        Patterns.match(String.class, carrierMetadata, 1),
+        Patterns.match(String.class, carrierMetadata, 2),
+        carrierMetadata
+    );
+    var carrier2 = pattern.invokeExact("foo", carrier);
+    assertEquals(2, (int) carrierMetadata.accessor(0).invokeExact(carrier2));
+  }
+
+  @Test
+  public void orFirstFalse() throws Throwable {
+    var carrierMetadata = new CarrierMetadata(methodType(Object.class, int.class));
+    var carrier = carrierMetadata.empty();
+
+    var pattern = Patterns.or(
+        Patterns.match(String.class, carrierMetadata, -1),
+        Patterns.match(String.class, carrierMetadata, 2),
+        carrierMetadata
+    );
+    var carrier2 = pattern.invokeExact("foo", carrier);
+    assertEquals(-1, (int) carrierMetadata.accessor(0).invokeExact(carrier2));
+  }
+
+  @Test
+  public void record_accessor() {
+
+  }
+
+  @Test
+  public void of() {
+  }
+
+
+
+
+}
